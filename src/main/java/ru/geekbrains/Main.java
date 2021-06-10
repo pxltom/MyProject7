b00d232834afcf671adc151eeaa458a0df725e7f
Artjom Islyamov <pixeltom@yandex.ru>
2021-06-10 17:36:51 +0300

    first window

--- src/main/java/ru/geekbrains/Main.java
+++ src/main/java/ru/geekbrains/Main.java
@@ -1,4 +1,9 @@
 package ru.geekbrains;
 
 public class Main {
+    public static void main(String[] args) {
+        window w = new window();
+    }
+
 }
+
--- src/main/java/ru/geekbrains/Window.java
+++
@@ -1,5 +0,0 @@
-package ru.geekbrains;
-
-public class Window {
-
-}
